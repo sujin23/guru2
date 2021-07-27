af526d17a2aea3779e847c2dbeca0de4e030f901
sujin23 <sujin@swu.ac.kr>
2021-07-27 14:38:54 +0900

    3페이지

---
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_result, menu)
+        return true
+    }
+}
